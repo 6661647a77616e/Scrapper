f8500368ed1784fdfe7444f00fb3725fdd61c8ba
faz1241543245398645 <111303147+faz1241543245398645@users.noreply.github.com>
2023-06-05 00:59:48 +0800

    Update GenBit.py
    bruteforce request until found one working link

--- GenBit.py
+++ GenBit.py
@@ -1,12 +1,20 @@
-function generateRandomString(length) {
-  const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  let result = '';
-  for (let i = 0; i < length; i++) {
-    const randomIndex = Math.floor(Math.random() * chars.length);
-    result += chars.charAt(randomIndex);
-  }
-  return result;
-}
+import requests
+import random
 
-const randomString = "bit.ly/" + generateRandomString(7);
-console.log(randomString)
+def generate_random_string(length):
+  chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  result = ''
+  for i in range(length):
+    random_index = random.randint(0, len(chars) - 1)
+    result += chars[random_index]
+  return result
+
+while True:
+  random_link = "https://bit.ly/" + generate_random_string(7)
+  response = requests.get(random_link)
+  if response.status_code == 200:
+    print(random_link,response)
+    break
+  else:
+    print(random_link,response)
+    continue
