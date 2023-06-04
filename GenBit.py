c7d97d209d58e6d816bb473669af41cfd0eceb07
faz1241543245398645 <111303147+faz1241543245398645@users.noreply.github.com>
2023-06-05 00:48:25 +0800

    Create GenBit.py

---
+++ GenBit.py
@@ -0,0 +1,12 @@
+function generateRandomString(length) {
+  const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * chars.length);
+    result += chars.charAt(randomIndex);
+  }
+  return result;
+}
+
+const randomString = "bit.ly/" + generateRandomString(7);
+console.log(randomString)
